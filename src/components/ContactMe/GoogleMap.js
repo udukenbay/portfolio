1fb635d3197af70a85d413590146ec49a711bb8e
u.dukenbay <urziya.dukenbay@gmail.com>
2022-03-30 23:10:17 +0600

    setState lat lng

--- src/components/ContactMe/GoogleMap.js
+++ src/components/ContactMe/GoogleMap.js
@@ -3,24 +3,60 @@ import GoogleMapReact from 'google-map-react';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
+// const Marker = (options) => {
+//   const [marker, setMarker] = React.useState();
+
+//   React.useEffect(() => {
+//     if (!marker)
+//       setMarker(new google.maps)
+//   })
+// };
+
 class GoogleMap extends Component {
-  static defaultProps = {
-    center: { lat: 43.26, lng: 76.93 },
-    zoom: 8
-  };
+  // static defaultProps = {
+  //   center: { lat: 43.26, lng: 76.93 },
+  //   zoom: 8
+  // };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      center: { lat: 37.78, lng: 122.42 },
+      zoom: 8,
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => this.setState({
+        center: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        },
+        zoom: 11
+      }),
+      err => console.log(err)
+    );
+
+    // navigator.geolocation.getCurrentPosition(function(position) {
+    //   console.log("Latitude is :", position.coords.latitude);
+    //   console.log("Longitude is :", position.coords.longitude);
+    // });
+  }
 
   render() {
-    return (
+      return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100%', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: "" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={this.state.center}
+          // defaultCenter={this.props}
+          defaultZoom={this.state.zoom}
         >
           <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
+            lat={this.state.center.lat}
+            lng={this.state.center.lng}
             text="My Marker"
           />
         </GoogleMapReact>
